6b95303285472a0a1696a63f0a6dc41772a7ea4b
allenhouu <126ahou@frhsd.com>
2025-10-24 09:25:03 -0400

    10/24

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import {useEffect, useState} from 'react'
+
+import './App.css'
+
+function App() {
+
+    const[username, setUsername] = useState('allen')
+    const[user, findUser] = useState<{login: string, avatar_url: string}[]>([]);
+
+
+    useEffect(() => {
+        fetch('https://api.github.com/search/users?q='+username+'+in:login')
+            .then(res => res.json())
+            .then(data => {
+                    findUser(data.items)
+                console.log(data)
+            })
+            .catch(err => console.log(err));
+    }, [username]);
+
+    return<>
+        <input id="search" placeholder="Type here to search"/>
+        <button onClick={() => {
+            let name = (document.getElementById('search') as HTMLInputElement).value;
+            setUsername(name);
+        }
+        }>Search</button>
+        {
+        user.map((item, i) =>
+        <div id="result" key={i}>
+            <img src = {item.avatar_url}/>
+            <span id="name">{item.login}</span>
+        </div>
+        )
+        }
+    </>
+
+}
+
+export default App
